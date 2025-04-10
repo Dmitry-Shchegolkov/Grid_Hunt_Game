b9d6b9630a25b27445ef4a8b3e79d9087a6725a2
Dmitry <dima.shchegolkov@gmail.com>
2025-04-10 17:32:34 +0300

    Add misc, resources, Main Change pom.xml

--- src/main/java/com/shchegolkov/App.java
+++
@@ -1,8 +0,0 @@
-package com.shchegolkov;
-
-public class App 
-{
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ src/main/java/com/shchegolkov/Main.java
@@ -0,0 +1,7 @@
+package com.shchegolkov;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("yes");
+    }
+}
